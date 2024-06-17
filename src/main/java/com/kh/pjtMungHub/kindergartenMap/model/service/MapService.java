d0c4a8e7e43bc7bdb50fac0ca0d54c54c6245754
jisoo0507 <jisoo0507@naver.com>
2024-06-17 15:51:19 +0900

    [INSERT]지도VO생성,지도조회메서드생성,mybatis별칭추가

---
+++ src/main/java/com/kh/pjtMungHub/kindergartenMap/model/service/MapService.java
@@ -0,0 +1,12 @@
+package com.kh.pjtMungHub.kindergartenMap.model.service;
+
+import java.util.ArrayList;
+
+import com.kh.pjtMungHub.kindergartenMap.model.vo.MapVO;
+
+
+
+public interface MapService {
+
+	public ArrayList<MapVO> selectMap();
+}
